9d421b48a2d56082a4fd74a80c8b4cd3bf4b72f2
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-12-14 16:39:33 +0100

    mapbox done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
-def home
-
+  def home
+    @users = User.all
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+        # info_window: render_to_string(partial: "info_window", locals: { user: user })
+      }
+    end
   end
 end
